29ffb080a9712d478c941d4d575ec92f42b72882
Hiago Mayk <hmayk@hotmail.com>
2015-12-09 05:38:16 -0300

    Cabelereiro

--- src/control/Cabeleireiro.java
+++ src/control/Cabeleireiro.java
@@ -5,32 +5,78 @@ import org.jcsp.lang.*;
 public class Cabeleireiro implements CSProcess {
 
 	private int id;
-	private final AltingChannelInput corte_chan;
-	private final AltingChannelInput lavagemC_chan;
-	private final AltingChannelInput lavagemCP_chan;
+	private final AltingChannelInputInt corte_chan_x_in;
+	private final AltingChannelInputInt penteado_chan_x_in;
+	private final AltingChannelInputInt lavagemC_chan_x_in;
+	private final AltingChannelInputInt lavagemCP_chan_x_in;
 	
-	public Cabeleireiro(int id, AltingChannelInput corte_chan, AltingChannelInput lavagemC_chan,
-			AltingChannelInput lavagemCP_chan) {
+	private final AltingChannelOutputInt corte_chan_x_out;
+	private final AltingChannelOutputInt penteado_chan_x_out;
+	private final AltingChannelOutputInt lavagemC_chan_x_out;
+	private final AltingChannelOutputInt lavagemCP_chan_x_out;
+	
+	private final AltingChannelInputInt corte_chan_i_in;
+	private final AltingChannelInputInt penteado_chan_i_in;
+	private final AltingChannelInputInt lavagemC_chan_i_in;
+	private final AltingChannelInputInt lavagemCP_chan_i_in;
+	
+	private final AltingChannelOutputInt corte_chan_i_out;
+	private final AltingChannelOutputInt penteado_chan_i_out;
+	private final AltingChannelOutputInt lavagemC_chan_i_out;
+	private final AltingChannelOutputInt lavagemCP_chan_i_out;
+	
+	public Cabeleireiro(int id, AltingChannelInputInt chanIn[], AltingChannelOutputInt chanOut[]) {
 		this.id = id;
-		this.corte_chan = corte_chan;
-		this.lavagemC_chan = lavagemC_chan;
-		this.lavagemCP_chan = lavagemCP_chan;
+		this.corte_chan_x_in = chanIn[0];
+		this.penteado_chan_x_in = chanIn[1];
+		this.lavagemC_chan_x_in = chanIn[2];
+		this.lavagemCP_chan_x_in = chanIn[3];
+		
+		this.corte_chan_x_out = chanOut[0];
+		this.penteado_chan_x_out = chanOut[1];
+		this.lavagemC_chan_x_out = chanOut[2];
+		this.lavagemCP_chan_x_out = chanOut[3];
+		
+		this.corte_chan_i_in = chanIn[4];
+		this.penteado_chan_i_in = chanIn[5];
+		this.lavagemC_chan_i_in = chanIn[6];
+		this.lavagemCP_chan_i_in = chanIn[7];
+		
+		this.corte_chan_i_out = chanOut[4];
+		this.penteado_chan_i_out = chanOut[5];
+		this.lavagemC_chan_i_out = chanOut[6];
+		this.lavagemCP_chan_i_out = chanOut[7];
 	}
 	
 	public void run() {
-		final Guard[] guards = new Guard[]{lavagemC_chan, lavagemCP_chan, corte_chan};
+		final Guard[] guards = new Guard[]{lavagemC_chan_x_in, lavagemCP_chan_x_in, penteado_chan_x_in};
 		Alternative alt = new Alternative(guards);
 		
 		final int C_LAVC = 0;
-		final int C_LAVCP = 1;
-		final int C_COR = 2;
+		final int C_PEN = 1;
+		final int C_LAVCP = 2;
 		
 		switch(alt.select()) {
 		case C_LAVC:
-			ChannelOutput ax;
-			AltingChannelInput ay;
+			lavagemC_chan_x_in.read();
+			lavagemC_chan_i_out.write(id);
 			
+			corte_chan_x_out.write(lavagemC_chan_x_in.read());
+			corte_chan_i_out.write(id);
+		
+		case C_PEN:
+			penteado_chan_x_in.read();
+			penteado_chan_i_out.write(id);
 			
+		case C_LAVCP:
+			lavagemC_chan_x_in.read();
+			lavagemC_chan_i_out.write(id);
+			
+			corte_chan_x_out.write(lavagemC_chan_x_in.read());
+			corte_chan_i_out.write(id);
+			
+			penteado_chan_x_out.write(lavagemC_chan_x_in.read());
+			penteado_chan_i_out.write(id);
 		}
 	}
 
